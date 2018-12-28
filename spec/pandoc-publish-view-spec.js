377a180ec567ab9c0a913e6db0478621b7e0f2c7
Mark Weber <maweber@dfwmac40n4htdg.paloaltonetworks.local>
2018-12-27 22:46:38 -0600

    Initial Commit

---
+++ spec/pandoc-publish-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PandocPublishView from '../lib/pandoc-publish-view';
+
+describe('PandocPublishView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
